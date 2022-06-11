b6f778ac96a6435813fcb7ffdc60ff37b8be73b5
Nikhil Rustagi <Nikhil.Rustagi1@dell.com>
2022-06-11 14:03:53 +0530

    minor change

--- src/app/offer/components/offer-grid/offer-grid.component.ts
+++ src/app/offer/components/offer-grid/offer-grid.component.ts
@@ -1,11 +1,23 @@
 import { OfferService } from './../../services/offer.service';
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTable } from '@angular/material/table';
 import { OfferGridDataSource, OfferGridItem } from './offer-grid-datasource';
 import { Offer } from '../../models/offer';
-import { merge, tap } from 'rxjs';
+import {
+  debounceTime,
+  distinctUntilChanged,
+  fromEvent,
+  merge,
+  tap,
+} from 'rxjs';
 
 @Component({
   selector: 'od-offer-grid',
@@ -16,10 +28,18 @@ export class OfferGridComponent implements OnInit, AfterViewInit {
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
   @ViewChild(MatTable) table!: MatTable<Offer>;
+  @ViewChild('input') input!: ElementRef;
   dataSource: OfferGridDataSource;
 
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
-  displayedColumns = ['quote#', 'quoteDesc', 'createdOn'];
+  displayedColumns = [
+    'quoteNumber',
+    'quoteDescription',
+    'createdBy',
+    'createdDate',
+    'validity',
+  ];
+  displayedColumnsWithExpand = [...this.displayedColumns, 'expand'];
 
   constructor(private _offerService: OfferService) {
     this.dataSource = new OfferGridDataSource(this._offerService);
@@ -27,22 +47,33 @@ export class OfferGridComponent implements OnInit, AfterViewInit {
   ngOnInit() {
     this.loadOffers();
   }
+  ngAfterViewInit() {
+    // server-side search
+    fromEvent(this.input.nativeElement, 'keyup')
+      .pipe(
+        debounceTime(150),
+        distinctUntilChanged(),
+        tap(() => {
+          this.paginator.pageIndex = 0;
+          this.loadOffers(this.input.nativeElement);
+        })
+      )
+      .subscribe();
+    // reset the paginator after sorting
+    this.sort.sortChange.subscribe(() => (this.paginator.pageIndex = 0));
+    // on sort or paginate events, load a new page
+    merge(this.sort.sortChange, this.paginator.page)
+      .pipe(tap(() => this.loadOffers()))
+      .subscribe();
+  }
 
-  loadOffers() {
+  loadOffers(filter: string = '') {
     return this.dataSource.loadOffers(
+      filter,
       this.sort?.active,
       this.sort?.direction,
       this.paginator?.pageIndex,
       this.paginator?.pageSize
     );
-  }
-
-  ngAfterViewInit() {
-    // reset the paginator after sorting
-    this.sort.sortChange.subscribe(() => (this.paginator.pageIndex = 0));
-
-    merge(this.sort.sortChange, this.paginator.page)
-      .pipe(tap(() => this.loadOffers()))
-      .subscribe();
   }
 }
